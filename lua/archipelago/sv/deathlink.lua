adc680a7a6fa1e33c84c1252c1680a4b2a550fc9
ChrisCj <chriscj136@gmail.com>
2025-11-03 12:00:15 +0100

    check if bounced packets even have a tags table when trying to figure out if they're a deathlink packet

--- lua/archipelago/sv/deathlink.lua
+++ lua/archipelago/sv/deathlink.lua
@@ -1,7 +1,7 @@
 hook.Add("AP_Bounced","GMAP_DeathLink", function(slot,packet) 
     -- currently checks if the packet has a deathlink tag on it to figure out if it's supposed to be a deathlink package
     -- this might cause issues if someone were to send a non-deathlink bounce package to all slots using it for some reason
-    if !packet.tags.DeathLink then return end
+    if !packet.tags or !packet.tags.DeathLink then return end
     local dat = packet.data
     print("Slot "..slot.ID.." received a DeathLink from "..dat.source.." ("..dat.cause..")")
     hook.Run("AP_DeathLink",slot,packet)
